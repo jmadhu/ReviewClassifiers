d7416b4ae3107781b6ac315786899d89e58aeb2a
Kevin Lee <kevinlee5@gmail.com>
2014-05-02 22:07:35 -0400

    initial code that doesnt really work qq

---
+++ src/Example.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+
+public class Example {
+    public boolean isPositive;
+    public HashMap<String, Integer> wordCounts;
+    
+    public Example() {
+        wordCounts = new HashMap<String, Integer>();
+    }
+    
+    public double[] getFeatures() {
+        return null;
+    }
+}
---
+++ src/Extractor.java
@@ -0,0 +1,69 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Extractor {
+    public static final int MAX_WORD_LENGTH = 20;
+    public static final String REVIEW_BEGIN_TOKEN = "review/text:";
+    public static final String REVIEW_END_TOKEN = "product/productId:";
+    public static final String SCORE_TOKEN = "review/score:";
+    public HashMap<String, Integer> vocabulary;
+    public ArrayList<Example> examples;
+    
+    public Extractor() {
+        vocabulary = new HashMap<String, Integer>();
+        examples = new ArrayList<Example>();
+    }
+    
+    /**
+     * extract vocabulary and examples from file at filepath
+     * 
+     * @param filepath
+     */
+    public void extract(String filepath) {
+        boolean buildingExample = false;
+        Example example = new Example();
+        try {
+            Scanner scanner = new Scanner(new File(filepath));
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                if (buildingExample && word.equals(REVIEW_END_TOKEN)) {
+                    buildingExample = false;
+                    examples.add(example);
+                    example = new Example();
+                } else {
+                    if (!buildingExample) {
+                        if (word.equals(SCORE_TOKEN)) {
+                            double score = Double.parseDouble(scanner.next());
+                            example.isPositive = score >= 2.5;
+                        } else if (word.equals(REVIEW_BEGIN_TOKEN)) {
+                            buildingExample = true;
+                        }
+                    } else {
+                        if (word.length() <= MAX_WORD_LENGTH) {
+                            Integer exampleCount = example.wordCounts.get(word);
+                            Integer vocabCount = vocabulary.get(word);
+                            example.wordCounts.put(word, exampleCount == null ? 1 : exampleCount + 1);
+                            vocabulary.put(word, vocabCount == null? 1 : vocabCount + 1);
+                        }
+                        // append if EOF is reached
+                        if (!scanner.hasNext()) {
+                            examples.add(example);
+                        }
+                    }
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+    public static void main(String[] args){
+        Extractor extractor = new Extractor();
+        extractor.extract("foods.txt");
+    }
+}
